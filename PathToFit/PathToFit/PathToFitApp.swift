28069823e64bb52a741640c9b8b8daf9271ba6ad
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-08-21 19:16:14 -0400

    Empty app file

---
+++ PathToFit/PathToFit/PathToFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathToFitApp.swift
+//  PathToFit
+//
+//  Created by Jeffrey Sweeney on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PathToFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
