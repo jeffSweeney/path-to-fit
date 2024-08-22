3a38d111cfb2a5f979757ebb1e8d703efcb56d41
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-08-22 07:41:03 -0400

    Initial navigation support

--- PathToFit/PathToFit/ContentView.swift
+++ PathToFit/PathToFit/ContentView.swift
@@ -7,14 +7,53 @@
 
 import SwiftUI
 
+enum HealthMetricContext: CaseIterable, Identifiable {
+    case steps
+    case weight
+    
+    var id: Self { self }
+    
+    var title: String {
+        switch self {
+        case .steps:
+            "Steps"
+        case .weight:
+            "Weight"
+        }
+    }
+    
+    var tint: Color {
+        switch self {
+        case .steps:
+            return .pink
+        case .weight:
+            return .indigo
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var selectedStat: HealthMetricContext = .steps
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 // MARK: - Entire Screen
                 VStack(spacing: 20) {
+                    // MARK: - Health Stat Context
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        ForEach(HealthMetricContext.allCases) { metric in
+                            Text(metric.title)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
                     // MARK: - Steps Card
-                    StepsCard()
+                    NavigationLink(value: selectedStat) {
+                        StepsCard()
+                    }
+                    .foregroundStyle(.secondary)
+                        
                     
                     // MARK: - Averages Card
                     AveragesCard()
@@ -22,7 +61,11 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                Text(metric.title)
+            }
         }
+        .tint(selectedStat.tint)
     }
 }
 
@@ -44,18 +87,15 @@ struct StepsCard: View {
                     
                     Text("Avg: 10k Steps")
                         .font(.caption)
-                        .foregroundStyle(.secondary)
                 }
                 
                 Spacer()
                 
                 Image(systemName: "chevron.right")
-                    .foregroundStyle(.secondary)
             }
             .padding(.bottom, 12)
             
             RoundedRectangle(cornerRadius: 12)
-                .foregroundStyle(.secondary)
                 .frame(height: 150)
         }
         .padding()
